9bea1cd4933821021421f69c9cd68dc4adea49d8
erleri <142582468+erleri@users.noreply.github.com>
2025-12-05 05:59:46 +0000

    Deploy site to GitHub Pages

---
+++ node_modules/@storybook/react-vite/dist/node/index.js
@@ -0,0 +1,19 @@
+import CJS_COMPAT_NODE_URL_plqvehhu1aj from 'node:url';
+import CJS_COMPAT_NODE_PATH_plqvehhu1aj from 'node:path';
+import CJS_COMPAT_NODE_MODULE_plqvehhu1aj from "node:module";
+
+var __filename = CJS_COMPAT_NODE_URL_plqvehhu1aj.fileURLToPath(import.meta.url);
+var __dirname = CJS_COMPAT_NODE_PATH_plqvehhu1aj.dirname(__filename);
+var require = CJS_COMPAT_NODE_MODULE_plqvehhu1aj.createRequire(import.meta.url);
+
+// ------------------------------------------------------------
+// end of CJS compatibility banner, injected by Storybook's esbuild configuration
+// ------------------------------------------------------------
+
+// src/node/index.ts
+function defineMain(config) {
+  return config;
+}
+export {
+  defineMain
+};
